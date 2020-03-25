da0c825da2e1d913c1d48fbb2c4105c98804974d
razornev <razornev@gmail.com>
2020-03-25 18:04:19 +0100

    Initial commit, linked GLFW library, is includable.

---
+++ cmake-build-debug/CMakeFiles/CUBEGINE.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/nbenc/CLionProjects/CUBEGINE/main.cpp" "C:/Users/nbenc/CLionProjects/CUBEGINE/cmake-build-debug/CMakeFiles/CUBEGINE.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../dependencies/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/nbenc/CLionProjects/CUBEGINE/cmake-build-debug/dependencies/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CUBEGINE.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CUBEGINE.dir/main.cpp.obj"
+  "CUBEGINE.exe"
+  "CUBEGINE.exe.manifest"
+  "CUBEGINE.pdb"
+  "libCUBEGINE.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CUBEGINE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
